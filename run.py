e4a504ec4558dd32493dd0bf42783a5b091c3a34
Leonhest <leon.hesthaug@gmail.com>
2026-03-13 19:17:28 +0100

    refactor: change default path to test

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from runner.grid_search import main
 import sys
 
-DEFAULT_EXP_PATH = "./experiments/tiles/"
+DEFAULT_EXP_PATH = "./experiments/test/"
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
